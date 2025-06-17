3189a27b2449a05323a0213f5e90f6817e589ad6
budi3312101075 <budiprayoga5103@gmail.com>
2025-06-17 21:32:20 +0700

    updated: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,48 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     "process.env": process.env,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+      },
+      manifest: {
+        name: "Excelitas DND",
+        short_name: "Excelitas DND",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+    allowedHosts: [
+      "902f-2001-448a-1021-1524-cde9-d4b3-7900-9358.ngrok-free.app",
+    ],
+  },
   resolve: {
     alias: {
       "@tailwindConfig": path.resolve(__dirname, "tailwind.config.js"),
